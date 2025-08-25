6fc6200b4c32a95306dbae96a1a34264fe0cc142
Lucasporter0 <lucasporter999@gmail.com>
2025-08-25 14:18:31 -0400

    Create Logos.tsx

---
+++ src/components/Logos.tsx
@@ -0,0 +1,13 @@
+// Path: src/components/Logos.tsx
+export function Logos() {
+  return (
+    <section className="opacity-70">
+      <div className="container-edge grid grid-cols-2 sm:grid-cols-4 gap-8 items-center">
+        <div className="text-center text-white/50">Shopify</div>
+        <div className="text-center text-white/50">Klaviyo</div>
+        <div className="text-center text-white/50">Vercel</div>
+        <div className="text-center text-white/50">Google</div>
+      </div>
+    </section>
+  );
+}
